4c281c9c4a517248e5060c1dff099f917b8b48ec
grzegorzKowaluk <grzkowaluk@gmail.com>
2024-03-27 14:37:09 +0100

    feat: added example model

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,150 @@
 use bevy::{
+    pbr::{CascadeShadowConfigBuilder, NotShadowCaster},
     prelude::*,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(
+            Startup,
+            (setup_camera_fog, setup_terrain_scene, setup_instructions),
+        )
+        .add_systems(Update, (toggle_system, move_camera))
         .run();
 }
 
-const X_EXTENT: f32 = 600.;
+fn setup_camera_fog(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-1.0, 0.1, 1.0)
+                .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+            ..default()
+        },
+        FogSettings {
+            color: Color::rgba(0.35, 0.48, 0.66, 1.0),
+            directional_light_color: Color::rgba(1.0, 0.95, 0.85, 0.5),
+            directional_light_exponent: 30.0,
+            falloff: FogFalloff::from_visibility_colors(
+                15.0, // distance in world units up to which objects retain visibility (>= 5% contrast)
+                Color::rgb(0.35, 0.5, 0.66), // atmospheric extinction color (after light is lost due to absorption by atmospheric particles)
+                Color::rgb(0.8, 0.844, 1.0), // atmospheric inscattering color (light gained due to scattering from the sun)
+            ),
+        },
+    ));
+}
 
-fn setup(
+fn move_camera(
+    keycode: Res<ButtonInput<KeyCode>>,
+    mut camera: Query<&mut Transform, With<Camera>>,
+) {
+    for mut transform in camera.iter_mut() {
+        if keycode.pressed(KeyCode::KeyD) {
+            transform.translation.x += 0.05;
+        }
+        if keycode.pressed(KeyCode::KeyA) {
+            transform.translation.x -= 0.05;
+        }
+        if keycode.pressed(KeyCode::KeyS) {
+            transform.translation.z += 0.05;
+        }
+        if keycode.pressed(KeyCode::KeyW) {
+            transform.translation.z -= 0.05;
+        }
+
+        if keycode.pressed(KeyCode::ArrowLeft) {
+            transform.rotate_y(0.05);
+        }
+        if keycode.pressed(KeyCode::ArrowRight) {
+            transform.rotate_y(-0.05);
+        }
+        if keycode.pressed(KeyCode::ArrowDown) {
+            transform.rotate_x(0.05);
+        }
+        if keycode.pressed(KeyCode::ArrowUp) {
+            transform.rotate_x(-0.05);
+        }
+    }
+}
+
+fn setup_terrain_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    // Configure a properly scaled cascade shadow map for this scene (defaults are too large, mesh units are in km)
+    let cascade_shadow_config = CascadeShadowConfigBuilder {
+        first_cascade_far_bound: 0.3,
+        maximum_distance: 3.0,
+        ..default()
+    }
+    .build();
 
-    let shapes = [
-        Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
-        Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
-        Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
-        Mesh2dHandle(meshes.add(Triangle2d::new(
-            Vec2::Y * 50.0,
-            Vec2::new(-50.0, -50.0),
-            Vec2::new(50.0, -50.0),
-        ))),
-    ];
-    let num_shapes = shapes.len();
-
-    for (i, shape) in shapes.into_iter().enumerate() {
-        // Distribute colors evenly across the rainbow.
-        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
-
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: shape,
-            material: materials.add(color),
-            transform: Transform::from_xyz(
-                // Distribute shapes from -X_EXTENT to +X_EXTENT.
-                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-                0.0,
-                0.0,
-            ),
+    // Sun
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            color: Color::rgb(0.98, 0.95, 0.82),
+            shadows_enabled: true,
             ..default()
-        });
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 0.0)
+            .looking_at(Vec3::new(-0.15, -0.05, 0.25), Vec3::Y),
+        cascade_shadow_config,
+        ..default()
+    });
+
+    // Terrain
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("models\\terrain\\Mountains.gltf#Scene0"),
+        ..default()
+    });
+
+    // Sky
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(2.0, 1.0, 1.0)),
+            material: materials.add(StandardMaterial {
+                base_color: Color::hex("888888").unwrap(),
+                unlit: true,
+                cull_mode: None,
+                ..default()
+            }),
+            transform: Transform::from_scale(Vec3::splat(20.0)),
+            ..default()
+        },
+        NotShadowCaster,
+    ));
+}
+
+fn setup_instructions(mut commands: Commands) {
+    commands.spawn(
+        TextBundle::from_section(
+            "Press Spacebar to Toggle Atmospheric Fog.\nPress S to Toggle Directional Light Fog Influence.",
+            TextStyle {
+                font_size: 20.0,
+                ..default()
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(12.0),
+            left: Val::Px(12.0),
+            ..default()
+        }),
+    );
+}
+
+fn toggle_system(keycode: Res<ButtonInput<KeyCode>>, mut fog: Query<&mut FogSettings>) {
+    let mut fog_settings = fog.single_mut();
+
+    if keycode.just_pressed(KeyCode::Space) {
+        let a = fog_settings.color.a();
+        fog_settings.color.set_a(1.0 - a);
+    }
+
+    if keycode.just_pressed(KeyCode::KeyL) {
+        let a = fog_settings.directional_light_color.a();
+        fog_settings.directional_light_color.set_a(0.5 - a);
     }
 }
